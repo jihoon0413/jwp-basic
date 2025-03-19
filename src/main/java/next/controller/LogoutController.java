a199371738ba8d017e9268d90b285bd96ef3fec9
jihoon <wlgnstls0413@naver.com>
2025-03-19 22:21:21 +0900

    jsp 연결 성공

--- src/main/java/next/controller/LogoutController.java
+++ src/main/java/next/controller/LogoutController.java
@@ -1,19 +1,22 @@
-package next;
+package next.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/users/logout")
+public class LogoutController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("Hello World!");
+        HttpSession session = req.getSession();
+        session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
+        resp.sendRedirect("/");
     }
 }
