400bfbfecf3e26a4065d5fea641eb1c19dee0b48
jihoon <wlgnstls0413@naver.com>
2025-07-17 14:24:08 +0900

    chore

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -3,7 +3,6 @@ package core.mvc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
