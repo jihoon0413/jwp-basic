9f655ad62a20d208fd82f801c33604d2e94e4c64
jihoon <wlgnstls0413@naver.com>
2025-08-06 21:05:28 +0900

    CH10-새로운 매핑 구조에 맞는 controller추가

--- src/main/java/core/mvc/LegacyHandlerMapping.java
+++ src/main/java/core/mvc/LegacyHandlerMapping.java
@@ -12,18 +12,17 @@ import java.util.Map;
 public class LegacyHandlerMapping implements HandlerMapping {
     private static Map<String, Controller> controllers = new HashMap<>();
 
-    static {
+    public void initialize() {
         controllers.put("/home", new HomeController());
-        controllers.put("/users/loginForm", new ForwardController("/user/login.jsp"));
-        controllers.put("/users/form", new ForwardController("/user/form.jsp"));
+//        controllers.put("/users/loginForm", new ForwardController("/user/login.jsp"));
         controllers.put("/qna/form", new ForwardController("/qna/form.jsp"));
-        controllers.put("/users/login", new LoginController());
-        controllers.put("/users/create", new CreateUserController());
-        controllers.put("/users/logout", new LogoutController());
-        controllers.put("/users", new ListUserController());
-        controllers.put("/users/updateForm", new UpdateUserFormController());
-        controllers.put("/users/update", new UpdateUserController());
-        controllers.put("/users/profile", new ProfileController());
+//        controllers.put("/users/login", new LoginController());
+//        controllers.put("/users/create", new CreateUserController());
+//        controllers.put("/users/logout", new LogoutController());
+//        controllers.put("/users", new ListUserController());
+//        controllers.put("/users/updateForm", new UpdateUserFormController());
+//        controllers.put("/users/update", new UpdateUserController());
+//        controllers.put("/users/profile", new ProfileController());
         controllers.put("/qna/show", new ShowController());
         controllers.put("/qna/create", new CreateQuestionController());
         controllers.put("/qna/delete", new DeleteQuestionController());
